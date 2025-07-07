9ecab2ed978f27ba4da459d56a91e89cee531ee1
Rifky Kurniawan Putra <krifky14@gmail.com>
2025-07-08 00:41:40 +0700

    fix: add missing newline at end of file and retain comment

--- src/users/users.repository.interface.ts
+++ src/users/users.repository.interface.ts
@@ -1,7 +1,7 @@
-import { CreateUserDto } from "./dto/create-user.input";
-import { User } from "./entities/user.entity";
+import { CreateUserDto } from './dto/create-user.input';
+import { User } from './entities/user.entity';
 
 export interface UsersRepositoryItf {
-    getAll(): User[];
-    create(createDto: CreateUserDto): User;
+getAll(): User[];
+create(createDto: CreateUserDto): User;
 }
